908e4ddfcff3688772baec34289e33f783007a7f
ChiseWaguri <lqff.yt@gmail.com>
2025-03-05 18:33:39 +0700

    build.sh: attempt to fix clang download

--- build.sh
+++ build.sh
@@ -99,7 +99,7 @@ if [[ ! -x $CLANG_PATH/bin/clang || ! -f $CLANG_PATH/VERSION || "$(cat $CLANG_PA
 
     if [[ $USE_AOSP_CLANG == "true" || $CLANG_URL == *.tar.* ]]; then
         mkdir -p "$CLANG_PATH"
-        wget -q "$CLANG_URL" && tar -xf ./*.tar.* -C "$CLANG_PATH/" && rm ./*.tar.*
+        wget -qO clang-tarball "$CLANG_URL" && tar -xf clang-tarball -C "$CLANG_PATH/" && rm ./*.tar.*
     else
         git clone -q --depth=1 -b "$CUSTOM_CLANG_BRANCH" "$CLANG_URL" "$CLANG_PATH"
     fi
